6d54bdb48ecbabf9b88b23815b95b57d2c10e31a
Victor Guadalupe Lopez Cebreros <victorproroblox24@gmail.com>
2026-04-13 02:03:52 -0600

    Arrglando el match para las empresas

--- backend/src/routes/vacantesRoutes.js
+++ backend/src/routes/vacantesRoutes.js
@@ -6,12 +6,13 @@ const verificarToken = require('../middlewares/authMiddleware');
 router.get('/dashboard', verificarToken, vacantesController.getDashboardCompleto);
 router.get('/perfil-info', verificarToken, vacantesController.obtenerPerfilEmpresa);
 
+router.get('/match-estudiantes', verificarToken, vacantesController.realizarMatchEstudiantes);
+
+// Rutas dinámicas (Las que tienen :id SIEMPRE VAN ABAJO)
 router.post('/', verificarToken, vacantesController.crearVacante);           
 router.get('/:id', verificarToken, vacantesController.obtenerVacante);       
 router.put('/:id', verificarToken, vacantesController.actualizarVacante);    
 router.delete('/:id', verificarToken, vacantesController.eliminarVacante);  
 router.put('/postulaciones/:id_postulacion/aceptar', verificarToken, vacantesController.aceptarPostulante); 
-
-router.get('/match-estudiantes', verificarToken, vacantesController.realizarMatchEstudiantes);
 
 module.exports = router;
