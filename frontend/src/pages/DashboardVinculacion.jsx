044c5a8afb33ffbfa7c9bf14a5ca1e525618e748
Victor Guadalupe Lopez Cebreros <victorproroblox24@gmail.com>
2026-04-14 17:46:10 -0600

    profesores subir proyecto2

--- frontend/src/pages/DashboardVinculacion.jsx
+++ frontend/src/pages/DashboardVinculacion.jsx
@@ -296,6 +296,7 @@ export default function DashboardVinculacion() {
           <>
             <div className="topbar">
               <div className="topbar-left-wrap">
+                {/* 🟢 BOTÓN HAMBURGUESA 🟢 */}
                 <button className="hamburger-btn" onClick={() => setIsMobileMenuOpen(true)}>
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                 </button>
